fdd476a0c7152d69c904196d89f5f6bc6fc96909
docbot@travis <doc bot>
2015-03-16 04:56:35 +0000

    rebuild pages at 2dd56bb

---
+++ implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ordered_iter'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;A, B&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='ordered_iter/struct.InnerJoinMapIterator.html' title='ordered_iter::InnerJoinMapIterator'>InnerJoinMapIterator</a>&lt;A, B&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;A, B&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='ordered_iter/struct.InnerJoinSetIterator.html' title='ordered_iter::InnerJoinSetIterator'>InnerJoinSetIterator</a>&lt;A, B&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;MapIter, SetIter&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='ordered_iter/struct.InnerJoinMapSetIterator.html' title='ordered_iter::InnerJoinMapSetIterator'>InnerJoinMapSetIterator</a>&lt;MapIter, SetIter&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;A, B&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='ordered_iter/struct.OuterJoinIterator.html' title='ordered_iter::OuterJoinIterator'>OuterJoinIterator</a>&lt;A, B&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/ordered_iter/trait.OrderedSetIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ordered_iter'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
